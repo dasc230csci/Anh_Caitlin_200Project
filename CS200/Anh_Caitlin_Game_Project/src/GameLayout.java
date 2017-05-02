2bde96109061970c5541dfd6394c4efb6cdf2a3f
ABTRAN <abtran@viewlincsci27.ad.csbsju.edu>
2017-05-02 10:56:51 -0500

    changes for GUI and riddle added

--- CS200/Anh_Caitlin_Game_Project/src/GameLayout.java
+++ CS200/Anh_Caitlin_Game_Project/src/GameLayout.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -20,6 +21,13 @@ public class GameLayout {
 	private Map<String, Set<String>> connection;
 	private Map<String, LocationDescription> descriptions;
 	private PlanetsDescription planets;
+	private int resources, gBomb, key, lightSaber, offering, clueNumber;
+	private String clue;
+	private String riddle;
+	private String[] riddleMaster;
+	private String[] clueMaster;
+	private Random random;
+	
 	public GameLayout(){
 		this.connection = new HashMap<String, Set<String>>();
 		this.descriptions = new HashMap<String, LocationDescription>();
@@ -27,7 +35,48 @@ public class GameLayout {
 	public GameLayout(String fileName1, String fileName2) throws FileNotFoundException{
 		this.connection = new HashMap<String, Set<String>>();
 		this.descriptions = new HashMap<String, LocationDescription>();
+		this.resources = 250;
+		this.gBomb = 0;
+		this.key = 0;
+		this.lightSaber = 0;
+		this.offering = 0;
+		this.clue ="";
+		this.riddle="";
+		this.clueNumber=0;
 		this.planets= new PlanetsDescription();
+		this.random = new Random();
+		this.riddleMaster = new String[15];
+		this.riddleMaster[0]="Is an older one-hundred dollar bill worth more than a newer one?";
+		this.riddleMaster[1]="What building has the most stories?";
+		this.riddleMaster[2]="Railroad crossing without any cars. Can you spell that without any R's?(Please spell in the form of C-A-T. Thank you)";
+		this.riddleMaster[3]="How many apples grow on a tree?";
+		this.riddleMaster[4]="A truck driver is going opposite traffic on a one-way street. A police officer sees him but doesn't stop him.Why didn't the police officer stop him?";
+		this.riddleMaster[5]="A man was sitting on his couch and searching for the English Channel. How did he find it?";
+		this.riddleMaster[6]="Which is correct 'The yolk of the egg is white' or 'The egg yolk is white?'";
+		this.riddleMaster[7]="I am the only organ that named myself.What am I?";
+		this.riddleMaster[8]="Why can't the Tyrannosaurus rex clap?";
+		this.riddleMaster[9]="What kind of room has no doors or windows?";
+		this.riddleMaster[10]="Why did the hamburger go to the gym?";
+		this.riddleMaster[11]="Why would a hair stylist rather cut the hair of two brunettes than one red head?";
+		this.riddleMaster[12]="Why was the chef embarrassed?";
+		this.riddleMaster[13]="Which is the most curious letter?";
+		this.riddleMaster[14]="Which month has 28 days?";
+		this.clueMaster =  new String[15];
+		this.clueMaster[0]="Read all the word carefully, one word could have different meaning";
+		this.clueMaster[1]="The library has a lot of books and books have a lot of stories to tell, don't they?";
+		this.clueMaster[2]="Answer what is asked?";
+		this.clueMaster[3]="all the fruit must come from a tree";
+		this.clueMaster[4]="Just because you have a car, it does not mean you drive it all the time";
+		this.clueMaster[5]="The map is more useful than you think";
+		this.clueMaster[6]="the color of the egg yolk is yellow";
+		this.clueMaster[7]="If you think your brain is weird, it is basically just your brain think itself as weird.";
+		this.clueMaster[8]="How long ago did the dinosaur went extinc?";
+		this.clueMaster[9]="Could the mushroom be consider a room or no?";
+		this.clueMaster[10]="I would like to go to the gym, my buns are not looking good right now";
+		this.clueMaster[11]="if your mom give you the choice between $200 versus only $100, you would take the 200 right?";
+		this.clueMaster[12]="Salad dressing is actually not the sald putting on clothe but the source to put into the salad";
+		this.clueMaster[13]="do you text your friend 'Why?' or 'Y?'";
+		this.clueMaster[14]="Evermonth in the year has atleast 28 days.";
 		planets.begin();
 		File file2 = planets.writeFile(fileName2);
 	    try{
@@ -109,4 +158,90 @@ public class GameLayout {
 		    }
 	}
 	
+	public void explore(String planetName){
+		if(this.descriptions.get(planetName).getAlien().equals("Good Alien")||this.descriptions.get(planetName).getAlien().equals("Yoda") ){
+		this.resources += Integer.parseInt(this.descriptions.get(planetName).getResources());
+		if(this.descriptions.get(planetName).getGravityBomb().equals("Gravity Bomb")){
+		this.gBomb ++;
+		}
+		if(this.descriptions.get(planetName).getWormHoleKey().equals("Wormhole Key")){
+		this.key ++;
+		}
+		if(this.descriptions.get(planetName).getPeaceOffering().equals("Peace Offering")){
+		this.offering ++;
+		}
+		if(this.descriptions.get(planetName).getLightSaber().equals("Light Saber")){
+			this.lightSaber ++;
+		}
+		else if(this.descriptions.get(planetName).getLightSaber().equals("clue")){
+			this.clueNumber ++;
+		}
+		}
+		else if(this.descriptions.get(planetName).getAlien().equals("Bad Alien")){
+			this.resources += Integer.parseInt(this.descriptions.get(planetName).getResources());
+			this.riddle=this.descriptions.get(planetName).getLightSaber();
+		}
+	}
+	public int getClueNumber(){
+		return this.clueNumber;
+	}
+	public int getResources(){
+		return this.resources;
+	}
+	
+	public int getGBomb(){
+		return this.gBomb;
+	}
+	
+	public int getKey(){
+		return this.key;
+	}
+	
+	public int getOffering(){
+		return this.offering;
+	}
+	
+	public int getYodaGift(){
+		return this.lightSaber;
+	}
+	
+	public String getRiddle(){
+		return this.riddle;
+	}
+	
+	public String getClue(){
+		return this.clue+"\n";
+	}
+	
+	public void setResources(int amount){
+		this.resources+=amount;
+	}
+	
+	public void setGBomb(int amount){
+		this.gBomb+=amount;
+	}
+	
+	public void setKey(int amount){
+		this.key+=amount;
+	}
+	
+	public void setOffering(int amount){
+		this.offering+=amount;
+	}
+	
+	public void setYodaGift(int amount){
+		this.lightSaber+=amount;
+	}
+	
+	public void setRiddle(String riddle){
+		this.riddle = riddle;
+	}
+	
+	public void setClue(String clue){
+		this.clue=clue;
+	}
+	
+	public void setClueNumber(int number){
+		this.clueNumber+=number;
+	}
 }
